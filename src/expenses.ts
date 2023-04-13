5e4475fbf410780c73fed4ad0a7e7c4ad390aaf6
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2023-04-13 13:45:42 -0300

    Adiciona rota que busca despesas por id

--- src/expenses.ts
+++ src/expenses.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { Request, Response } from 'express';
 import loadDatabase from './utils/loadDatabase';
+import { IExpense } from './interfaces/expense';
 
 const route = express.Router();
 
@@ -8,6 +9,19 @@ route.get('/', (req: Request, res: Response) => {
     const expenses = loadDatabase('expenses');
 
     res.status(200).json(expenses);
+});
+
+route.get('/:id', (req: Request, res: Response) => {
+    const expenses = loadDatabase('expenses');
+    const id = req.params.id;
+
+    const expense: IExpense = expenses.find((item: IExpense) => item.id === id);
+
+    if(!expense){
+        res.status(404).json({message: 'Despesa não encontrada'})
+    }
+
+    res.status(200).json(expense);
 });
 
 export default route;
