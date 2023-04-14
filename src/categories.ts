ae13f40094ae62f9c9325b62a0ee12118cbf83ac
Pablo <pablomreinheimer@gmail.com>
2023-04-13 20:10:53 -0400

    Adiciona a rota get que lista as categorias

--- src/categories.ts
+++ src/categories.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { Request, Response } from 'express';
 import loadDatabase from './utils/loadDatabase';
+import { ICategory } from './interfaces/category';
 
 const route = express.Router();
 
 route.get('/', (req: Request, res: Response) => {
-    const categorias = loadDatabase('categories');
+    const categorias:ICategory[] = loadDatabase('categories');
 
     res.status(200).json(categorias);
 });
