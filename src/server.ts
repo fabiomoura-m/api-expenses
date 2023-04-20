d8d04df1c3c7ed7ca793aa862addae99b35c2cb8
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2023-04-20 20:32:07 -0300

    Adiciona Cors ao projeto

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import categoriesRoute from './categories';
 import expensesRoute from './expenses';
 import usersRoute from './users';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/categories', categoriesRoute);
 app.use('/expenses', expensesRoute);
