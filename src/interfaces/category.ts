5afec2da8c8ff493d8d6d5d8aa9984fff7f0310f
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2023-04-20 21:29:26 -0300

    Adiciona na rota get de usuario uma nova propriedade
    Adiciona a propriedade _categories em todos usuarios buscados, onde mostra as despesas separadas por categorias

--- src/interfaces/category.ts
+++ src/interfaces/category.ts
@@ -1,7 +1,13 @@
+import { IExpenseForUser } from './expense';
+
 export interface ICategory {
     id: string;
     name: string;
 }
 export interface IBodyCategory {
     name: string;
+}
+
+export interface ICategoryWithExpenses extends ICategory {
+    _expenses: IExpenseForUser[];
 }
--- src/users.ts
+++ src/users.ts
@@ -1,14 +1,14 @@
-
-import express, { request, response } from "express";
-import { Request, Response } from "express";
-import loadDatabase from "./utils/loadDatabase";
-import { searchUser } from "./utils/searchUser";
-import { IUser } from "./interfaces/user";
-import { bodySchemaUser } from "./schemas/users";
-import messages from "./enums/messages";
-import generateRandomNumber from "./utils/generateRandomId";
-import saveDataInJson from "./utils/saveDataInJson";
-import { IExpense } from "./interfaces/expense";
+import express, { request, response } from 'express';
+import { Request, Response } from 'express';
+import loadDatabase from './utils/loadDatabase';
+import { searchUser } from './utils/searchUser';
+import { IUser } from './interfaces/user';
+import { bodySchemaUser } from './schemas/users';
+import messages from './enums/messages';
+import generateRandomNumber from './utils/generateRandomId';
+import saveDataInJson from './utils/saveDataInJson';
+import { IExpense } from './interfaces/expense';
+import { ICategory, ICategoryWithExpenses } from './interfaces/category';
 
 const route = express.Router();
 
@@ -19,81 +19,104 @@ route.get('/', (req: Request, res: Response) => {
 
 route.get('/:id', (req: Request, res: Response) => {
     const users: IUser[] = loadDatabase('users');
+    let categories: ICategory[] = loadDatabase('categories');
     const id: string = req.params.id;
-    const result = searchUser(id, users);
-    if (result == undefined) {
+    const user = searchUser(id, users);
+    if (user == undefined) {
         return res.status(404).json({ message: messages.userNotFound });
     }
-    return res.status(200).json(result);
+
+    const categoriesWithExpenses: ICategoryWithExpenses[] = categories.map(
+        category => {
+            return {
+                ...category,
+                _expenses: []
+            };
+        }
+    );
+
+    user._expenses.forEach(expense => {
+        const category = categoriesWithExpenses.find(
+            category => category.id === expense.categoryID
+        );
+        if (category) {
+            category._expenses.push(expense);
+        }
+    });
+
+    const newUserWithCategories = {
+        ...user,
+        _categories: [...categoriesWithExpenses]
+    };
+
+    return res.status(200).json(newUserWithCategories);
 });
 
-route.post("/", (req: Request, res: Response) => {
-  const users: IUser[] = loadDatabase("users");
-  const data = req.body;
-  const randomNumber = generateRandomNumber();
-  const newID = `user_${randomNumber}`;
-  const newUser: IUser = {
-    id: newID,
-    name: data.name,
-    lastName: data.lastName,
-    email: data.email,
-    _expenses: [],
-  };
+route.post('/', (req: Request, res: Response) => {
+    const users: IUser[] = loadDatabase('users');
+    const data = req.body;
+    const randomNumber = generateRandomNumber();
+    const newID = `user_${randomNumber}`;
+    const newUser: IUser = {
+        id: newID,
+        name: data.name,
+        lastName: data.lastName,
+        email: data.email,
+        _expenses: []
+    };
 
-  const { error } = bodySchemaUser.validate(data);
+    const { error } = bodySchemaUser.validate(data);
 
-  if (error) {
-    return res.status(400).json({ message: messages.invalidBody });
-  }
-  users.push(newUser);
-  saveDataInJson(users, "users");
-  res.status(200).json({
-    id: newID,
-    name: data.name,
-    lastName: data.lastName,
-    email: data.email,
-  });
-});
-
-route.put("/:userID", (req: Request, res: Response) => {
-  const users: IUser[] = loadDatabase("users");
-  const expenses: IExpense[] = loadDatabase("expenses");
-  const data = req.body;
-  const userID: string = req.params.userID;
-  const user = users.find((user) => user.id == userID);
-
-  if (!user) {
-    return res.status(404).json({ message: messages.userNotFound });
-  }
-  const newUser = {
-    id: user.id,
-    name: data.name ?? user.name,
-    lastName: data.lastName ?? user.lastName,
-    email: data.email ?? user.email,
-    _expenses: user._expenses,
-  };
-  const indexUser = users.findIndex((user) => user.id == userID);
-  users[indexUser] = newUser;
-
-  const newUserForExpense = {
-    id: user.id,
-    name: data.name ?? user.name,
-    lastName: data.lastName ?? user.lastName,
-    email: data.email ?? user.email,
-  };
-  
-  expenses.forEach((expense)=>{
-
-    if(expense.userID === userID){
-      expense._user = newUserForExpense
+    if (error) {
+        return res.status(400).json({ message: messages.invalidBody });
     }
-    
-  });
+    users.push(newUser);
+    saveDataInJson(users, 'users');
+    res.status(200).json({
+        id: newID,
+        name: data.name,
+        lastName: data.lastName,
+        email: data.email
+    });
+});
 
-  saveDataInJson(expenses, "expenses")
-  saveDataInJson(users, "users");
+route.put('/:userID', (req: Request, res: Response) => {
+    const users: IUser[] = loadDatabase('users');
+    const expenses: IExpense[] = loadDatabase('expenses');
+    const data = req.body;
+    const userID: string = req.params.userID;
+    const user = users.find(user => user.id == userID);
 
-  res.status(200).json(newUserForExpense);
+    if (!user) {
+        return res.status(404).json({ message: messages.userNotFound });
+    }
+    const newUser = {
+        id: user.id,
+        name: data.name ?? user.name,
+        lastName: data.lastName ?? user.lastName,
+        email: data.email ?? user.email,
+        _expenses: user._expenses
+    };
+    const indexUser = users.findIndex(user => user.id == userID);
+    users[indexUser] = newUser;
+
+    const newUserForExpense = {
+        id: user.id,
+        name: data.name ?? user.name,
+        lastName: data.lastName ?? user.lastName,
+        email: data.email ?? user.email
+    };
+
+    expenses.forEach(expense => {
+        if (expense.userID === userID) {
+            expense._user = newUserForExpense;
+        }
+    });
+
+    saveDataInJson(expenses, 'expenses');
+    saveDataInJson(users, 'users');
+
+    res.status(200).json(newUserForExpense);
 });
 
 route.delete('/:userID', (req: Request, res: Response) => {
